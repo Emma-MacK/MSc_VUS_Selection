761c85ec71eca1e09d65a92706a8cbaed4593c2d
Emma-MacK <113516771+Emma-MacK@users.noreply.github.com>
2025-01-07 12:01:40 +0000

    Remove unnecessary comments

--- R_scripts/Permutation_Bap1__R_script.R
+++ R_scripts/Permutation_Bap1__R_script.R
@@ -12,34 +12,28 @@ library(stats)
 set.seed(1)  
 
 # want to read in previous functional studies information 
-bap1_funt <- readxl::read_excel("extended_data_1_(sge_bap1_dataset).xlsx", sheet = "extended_data_1",  col_names = FALSE,)
+bap1_funct <- readxl::read_excel("extended_data_1_(sge_bap1_dataset).xlsx", sheet = "extended_data_1",  col_names = FALSE,)
 
 # get correct columns name from row 3
 
-colnames(bap1_funt) <- bap1_funt[3,]
-bap1_funt <- bap1_funt[-1:-3, ] 
-
+colnames(bap1_funct) <- bap1_funt[3,]
+bap1_funt <- bap1_funct[-1:-3, ] 
 
 # get rows that coorespond to my variants
  
 my_variants <- c("3_52408479_G_A", "3_52409837_C_G", "3_52408046_A_C", 
                  "3_52406252_C_T", "3_52404500_A_C", "3_52409600_C_T")
-
-
-rows_of_my_variants <- bap1_funt[bap1_funt$chrom_pos_ref_alt %in% my_variants,]
+rows_of_my_variants <- bap1_funct[bap1_funct$chrom_pos_ref_alt %in% my_variants,]
 
 my_variants_mean <-  mean(as.numeric(rows_of_my_variants$functional_score))
+
 # get functional scores only
-# get absolute value so it is just change rather than depeleted or enriched
 
-funct_scores <- as.numeric(bap1_funt$functional_score)
+funct_scores <- as.numeric(bap1_funct$functional_score)
 
-# I have 6 variants, so randomly select 6?
-
-# permutation 
+# permutation randomly select 6, calc diff of sample average from dataset mean
 
 n_entries <- length(funct_scores)
-
 bap1_random_selections <- data.frame()
 
 for (x in 1:100000) {
@@ -51,7 +45,7 @@ for (x in 1:100000) {
   bap1_random_selections <- rbind(bap1_random_selections, samples)
 }
 
-
+# informative column names
 colnames(bap1_random_selections) <- c("selection 1", "selection 2", 
                                       "selection 3", "selection 4", 
                                       "selection 5", "selection 6",
@@ -59,9 +53,10 @@ colnames(bap1_random_selections) <- c("selection 1", "selection 2",
 
 
 ci95<-hdi(bap1_random_selections$mean_diff)
-# what is score of my selection
 
-plot_post_SE1 <- ggplot(bap1_random_selections, aes(x=mean_diff)) +
+# create plot
+
+plot_mean_diff <- ggplot(bap1_random_selections, aes(x=mean_diff)) +
   geom_histogram(color="darkblue", fill="lightblue", bins = 50) +
   geom_vline(aes(xintercept = mean(bap1_random_selections$mean_diff)), color = "blue") +
   geom_vline(xintercept = as.numeric(ci95[1:2]), linetype="dashed",
@@ -71,9 +66,7 @@ plot_post_SE1 <- ggplot(bap1_random_selections, aes(x=mean_diff)) +
   ggtitle("Bap 1 random selection mean diff in functional scores")
   
   
-plot_post_SE1
+plot_mean_diff
 
 
-# p value 
 
-#  use the distribution to obtain a p-value for our mean-difference by counting how many permuted mean-differences are larger than the one we observed in our actual data. We can then divide this by the number of items in our permutation distribution (i.e., 2000 from our call to replicate, above):
